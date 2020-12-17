9eddc71c507a7ca87477dd1c7e869f0374637de6
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-12-17 22:43:51 +0100

    Solve exercise "Acronym" (cont.)
    Replaced `#chars`+`#first` by `#chr`.

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,6 +1,6 @@
 class Acronym
   def self.abbreviate(sequence)
     raise "Expecting a string for parameter `sequence`: {sequence.inspect}" unless sequence.kind_of?(String)
-    sequence.split(/\W+/).map{ |e| e.chars.first.upcase }.join()
+    sequence.split(/\W+/).map{ |e| e.chr.upcase }.join()
   end
 end
